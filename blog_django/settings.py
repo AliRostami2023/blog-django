cc5275a4bbfcb19d8badc115c0a410e375fb4582
Ali Rostami <AliRostami1253@gmail.com>
2024-05-06 21:45:50 +0330

    update

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -90,9 +90,13 @@ WSGI_APPLICATION = 'blog_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'ali112280',
+        'PASSWORD': '553399191',
+        'HOST': 'localhost',
+        'PORT': '5432', # default PostgreSQL port
+}
 }
 
 # Password validation
